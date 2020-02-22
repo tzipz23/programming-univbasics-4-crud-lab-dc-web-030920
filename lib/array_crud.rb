c8a51ec4641470a2cac31c2d21c2b1f429c8d179
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-22 02:11:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop("array")
+  array.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift("array")
+  array.shift("arrays!")
 end
 
 def retrieve_element_from_index(array, index_number)
